51d0ec8684e214558b2e8651d5d7ce1ffab8e92d
BarelyAnXer <christiangazzingan38@gmail.com>
2024-01-31 19:19:38 +0800

    initial commit done 2 pages

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:blood_drive/screens/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          color: Color(0xFF43A0E3),
+        ),
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 160.0),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.elliptical(2000.0, 1000.0),
+              bottomRight: Radius.elliptical(2000.0, 1000.0),
+            ),
+            color: Colors.white, // You can set your desired background color
+          ),
+          child: Image.asset('assets/logo.png'),
+        ),
+      ),
+    );
+  }
+}
